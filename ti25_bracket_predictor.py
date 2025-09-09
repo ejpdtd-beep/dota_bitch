176b34d9b39b0299bf140bd27102a9d05096da2b
ejpdtd-beep <ejpdtd@gmail.com>
2025-09-09 17:27:05 +1000

    Update ti25_bracket_predictor.py

--- ti25_bracket_predictor.py
+++ ti25_bracket_predictor.py
@@ -38,7 +38,7 @@ ROSTER_ISSUES = {"Tundra Esports": True}
 W_RECENT, W_GROUP, W_ROSTER = 0.5, 0.3, 0.2
 ROSTER_PENALTY = 0.08
 H2H_BOOST = 0.05
-FALCONS_BONUS = 0.02
+FALCONS_BONUS = 0.09
 
 def parse_input():
     group_wr = {}
